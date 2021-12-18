8704865715834ed7205f724d4ca0175ba2c1b210
Daniel Díaz <daniel0diiaz@gmail.com>
2021-12-18 17:34:30 +0100

    Añadidos métodos a la aplicación de Vue y función para recuento en vivo

--- decide/visualizer/static/liveresults/liveresults.js
+++ decide/visualizer/static/liveresults/liveresults.js
@@ -1,9 +1,8 @@
 (($) => {
     console.log(voting);
-
+    app.getVotingCount();
     setInterval(() => {
-        console.log(voting.postproc[0].votes);
+        console.log(`Refrescando resultados de la votación: ${voting.id}`);
+        //app.getVotingCount();
     }, 1000);
-
-    
 })(jQuery);
