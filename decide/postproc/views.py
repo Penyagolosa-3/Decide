7161e00526845959f35b23a4c51e62e5a7591757
José Manuel Escobar Ruesga (josescrue) <josescrue@alum.us.es>
2021-12-18 19:08:37 +0100

    Feature: Comenzada definición borda en views.py; #3
    Se ha comenzado a definir la función de recuento de borda. Así mismo, se ha añadido el tipo correspondiente a este algoritmo en la función post.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,24 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+    def groups(self, options):
+
+        
+
+    
+    def borda(self, options):
+
+        # Comprobamos que las opciones tienen el atributo groups, habiendo al menos 2 opciones
+        if(len(options) < 2):
+            res = {'message': 'No hay opciones suficientes para agrupar'}
+        elif not 'group' in options[0]:
+            res = {'message': 'Los votos no se pueden agrupar'}
+        else:
+            for opt in options:
+                # Añadimos a todas las opciones el atributo 'total'
+                opt['total'] = 0
+        
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -34,5 +52,7 @@ class PostProcView(APIView):
 
         if t == 'IDENTITY':
             return self.identity(opts)
+        elif t == 'BORDA':
+            return self.borda(opts)
 
         return Response({})
