91be2b60d0fa6fe51fba9012b80cf534b385c5c8
marmarave <marmarave@alum.us.es>
2021-12-20 17:53:21 +0100

    feature: Creación modelo rol; #4
    He añadido la asignación de roles en la parte de los modelos de la autentificación

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+from enum import Enum
 
 # Create your models here.
+class Rol(Enum):
+    Cliente = 1;
+    Vendedor = 2;
+
+class Roll(models.Model):
+    user_id = models.PositiveIntegerField()
+    rol = Rol
+    
+    
