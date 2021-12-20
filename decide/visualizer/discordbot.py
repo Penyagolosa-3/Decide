f1c03eb6ca3d791841f2c75fdf18116c5f389e00
luirodvid <er5lui@gmail.com>
2021-12-20 16:41:03 +0100

    Feature: Añadido bot de telegram; #8

---
+++ decide/visualizer/discordbot.py
@@ -0,0 +1,45 @@
+import os
+import discord
+import json
+import requests
+from discord import Webhook,RequestsWebhookAdapter
+
+TOKEN = "OTIyMjgxOTIyNDAxNTQyMTU2.Yb_L-Q.WWWDujd3EPC2b-XAg-oySFMcVIA"
+#webhook_url = "https://discord.com/api/webhooks/922301093755633664/PIOHdcWk1OWIjwgdX9QPDntGR9zzrKYpyxy3iecQYUCovs9onMI6tCuCweKov1SYD9_K"
+#webhook_id = "922301093755633664"
+#webhook_token = "PIOHdcWk1OWIjwgdX9QPDntGR9zzrKYpyxy3iecQYUCovs9onMI6tCuCweKov1SYD9_K"
+#webhook = Webhook.partial(webhook_id,webhook_token,\
+#    adapter= RequestsWebhookAdapter())
+#webhook.send('Webhook enviado.')
+
+
+
+
+load_dotenv()
+client = discord.Client()
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "!visualizer" == message.content.lower().split()[0]:
+        voting_id = message.content.lower().split()[1]
+        if voting_id.isnumeric():
+            rtext = devuelveBaseDeDatos(voting_id)
+        else:
+            rtext = "Por favor introduzca un número valido"
+        await message.channel.send(rtext)
+    
+def devuelveBaseDeDatos(voting_id):
+    try:
+        r = mods.get('voting', params={'id': voting_id})
+        voting = json.dumps(r[0])
+    except:
+        return "Voto no finalizado"
+    voting = json.loads(voting)
+    text= "El voto {} ha tenido los siguientes resultados:\n\n".format(voting["name"])
+    for option in voting["postproc"]:
+        text+= "La opcion {} ha recibido {} votos\n".format(option["option"],option["postproc"])        
+    return text
+
+client.run(TOKEN)
