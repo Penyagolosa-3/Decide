4b3061806f7d19dae3b2c4e645197c53128be506
Daniel Díaz <daniel0diiaz@gmail.com>
2021-12-20 17:06:37 +0100

    Documentación de métodos, recuento total de votos y fix a peticiones 301

--- decide/visualizer/static/liveresults/liveresults.js
+++ decide/visualizer/static/liveresults/liveresults.js
@@ -1,8 +1,34 @@
 (() => {
     class VotingCount {
-        constructor(options) {
+        constructor(app, options, fetchInterval = 2500) {
+            this.totalVotes = 0;
+            this.app = app;
+            this.fetchInterval = fetchInterval;
             this.options = options;
+            this.intervalFetch();
         }
+
+        /*
+         * Descripción: Ejecuta de manera periódica según el parámetro fetchInterval una función de la app de Vue definida para recoger el recuento de votos de la base de datos
+         * Entrada: Ninguna
+         * Salida: Ninguna
+         */
+        intervalFetch() {
+            setInterval(() => {
+                console.log(`Refrescando resultados de la votación: ${voting.id}`);
+        
+                this.app.getVotingCount().then(data => {
+                    this.putOptionsValues(data);
+                });
+        
+            }, this.fetchInterval);
+        }
+
+        /*
+         * Descripción: recibe un recuento de votos, los filtra sumando cada votación por opción y actualiza la interfaz
+         * Entrada: un fetch de la tabla VotingCount
+         * Salida: Ninguna
+         */
         putOptionsValues(votingCountFetch) {
             let categorizedVoting = this.categorizeVotingCount(votingCountFetch);
             for(let v of categorizedVoting) {
@@ -15,11 +41,19 @@
                     }
                 }
             }
-            app.setOptions(this.options);
-            app.$forceUpdate();
+            this.app.setOptions(this.options);
+            this.app.setTotalVotes(this.totalVotes);
+            this.app.$forceUpdate();
         }
+
+        /*
+         * Descripción: recibe un recuento de votos y suma la cantidad de filas por cada respuesta a una opción de pregunta
+         * Entrada: un fetch de la tabla VotingCount
+         * Salida: Un array de objetos que identifican la opción de pregunta con la suma de las votaciones
+         */
         categorizeVotingCount(votingCount) {
-            let categorized = [];
+            let categorized = [],
+                totalVotes = 0;
 
             for(let v of votingCount) {
                 let option_id = v.option_id,
@@ -31,6 +65,7 @@
                     if(c.option_id==option_id) {
                         found = true;
                         c.count++;
+                        totalVotes += 1;
                     }
                 }
                 if(!found) {
@@ -40,20 +75,13 @@
                             count: 1
                         }
                     });
+                    totalVotes += 1;
                 }
             }
+            this.totalVotes = totalVotes;
             return categorized;
         }
     }
 
-    let votingCount = new VotingCount(voting.question.options);
-
-    setInterval(() => {
-        console.log(`Refrescando resultados de la votación: ${voting.id}`);
-        
-        app.getVotingCount().then(data => {
-            votingCount.putOptionsValues(data);
-        });
-
-    }, 1000);
+    new VotingCount(app, voting.question.options);
 })();
