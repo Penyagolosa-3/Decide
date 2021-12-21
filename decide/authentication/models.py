e15e3346602e602ad1c27d40ef0f5e84bbccc4ae
marmarave <marmarave@alum.us.es>
2021-12-21 16:30:35 +0100

    Fix: anadido meta a rol ; #4
    Anadido clase meta a rol

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -1,4 +1,24 @@
 from django.db import models
-
+from django.db.models import CheckConstraint
+from django.db.models.query_utils import Q
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
+
+class Authentication(models.Model):
+
+    class Rol(models.TextChoices):
+        SELLER = 'SL', _('Sheller')
+        CLIENT= 'CL', _('Client')
+
+    roles = models.CharField(
+        max_length=2,
+        choices=Rol.choices,
+        default=Rol.CLIENT,
+    )
+    
+    
+    class Meta:
+        constraints = [CheckConstraint(check=Q(roles__in=['Sheller', 'Client']), name="valid_roles")]
+
+
 
