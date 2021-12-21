45f149f7c6af0b2f4eee300ab073b409d75fb8cd
marmarave <marmarave@alum.us.es>
2021-12-21 17:21:03 +0100

    Fix: Arreglo rol ; #4
    Arreglo del modelo Rol

--- decide/authentication/models.py
+++ decide/authentication/models.py
@@ -3,21 +3,27 @@ from django.db.models.query_utils import Q
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
 
-class Authentication(models.Model):
-
-    class Rol(models.TextChoices):
-        SELLER = 'SE', _('Sheller')
-        CLIENT= 'CL', _('Client')
-
-    roles = models.CharField(
-        max_length=2,
-        choices=Rol.choices,
-        default=Rol.CLIENT,
+class Rol(models.Model):
+    USER_ROL = (
+    ('CLIENT', 'CLIENT'),
+    ('SELLER', 'SELLER')
     )
+    title = models.CharField(max_length=6, choices = USER_ROL, default='CLIENT')
+
+
+    # class Rol(models.TextChoices):
+    #     SELLER = 'SE', _('Sheller')
+    #     CLIENT= 'CL', _('Client')
+
+    # roles = models.CharField(
+    #     max_length=2,
+    #     choices=Rol.choices,
+    #     default=Rol.CLIENT,
+    # )
     
     
-    class Meta:
-        constraints = Q[models.CheckConstraint(check=Q(roles__in=['Sheller', 'Client']), name="valid_roles")]
+    # class Meta:
+    #     constraints = Q[models.CheckConstraint(check=Q(roles__in=['Sheller', 'Client']), name="valid_roles")]
 
 
 
