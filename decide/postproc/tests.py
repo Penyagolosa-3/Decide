ffeb9b2914c5fbb6954ce7a5f42695b4fc5c354f
Isamunval <isamunval@alum.us.es>
2021-12-22 00:51:51 +0100

    Test: Creación test negativo de paridad ; #24
    Creación de un test negativo del método de paridad

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -80,3 +80,28 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+    def testParidadError(self):
+        """
+            * Definicion: Test negativo por URL errónea
+            * Entrada: Votacion (Json)
+                - Option: Nombre del partido
+                - Number: Id de la opcion
+                - Votes: Numero de votos de esa votacion
+                - PostProc: Numero de personas que van a ir en la lista una vez aplicada la paridad
+                - Candidatos: Sexo e ID de los candidatos
+            * Salida: Codigo 404
+        """
+        data = {
+            'type': 'PARIDAD',
+            'options': [
+                {'option': 'Partido Unico', 'number': 1, 'votes': 5, 'postproc': 5, 'candidatos': [
+                 {'sexo': 'hombre', 'id': '1'}, {'sexo': 'mujer', 'id': '2'}, {
+                     'sexo': 'hombre', 'id': '3'}, {'sexo': 'mujer', 'id': '4'}, {'sexo': 'mujer', 'id': '5'}
+                 ]}
+            ]
+        }
+
+        response = self.client.post('/postproci/', data, format='json')
+        self.assertEqual(response.status_code, 404)
+
