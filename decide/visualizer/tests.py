9b292c92381e3ca22fd1ba3045622f48d1c3f7cd
luibelzan <luibelzan@outlook.com>
2022-01-03 20:40:13 +0100

    Test: Comenzando test_visualizer

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
+
+class VisualizerTestCase(StaticLiveServerTestCase):
+
+    def setUp(self):
+        self.base = BaseTestCase()
+        self.base.setUp()
+
+        options = webdriver.ChromeOptions()
+        #options.headless = True
+        self.driver = webdriver.Chrome(options=options)
+
+        super.setUp()
+
+    def tearDown():
+        super().tearDown()
+        self.driver.quit()
+
+        self.base.tearDown()
+
+
+
