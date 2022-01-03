4c57442fc452a93c3c8443d619ffb4fd1d0b7f8b
luibelzan <luibelzan@outlook.com>
2022-01-03 23:43:44 +0100

    Test: Funcionando metodos básicos (Visualizer)

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -7,25 +7,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-# Create your tests here.
+import sys
+sys.path.insert(1, '/home/luismi/Proyectos VS/Decide/decide/')
+#from base.tests import BaseTestCase
+import time
+import unittest
 
-class VisualizerTestCase(StaticLiveServerTestCase):
+class AdminTestCase(unittest.TestCase):
+
 
     def setUp(self):
-        self.base = BaseTestCase()
-        self.base.setUp()
+        self.driver = webdriver.Chrome() 
 
-        options = webdriver.ChromeOptions()
-        #options.headless = True
-        self.driver = webdriver.Chrome(options=options)
-
-        super.setUp()
-
-    def tearDown():
-        super().tearDown()
+    def test_simpleCorrectLogin(self):                
+        self.driver.get('http://www.localhost:8000/admin/')
+        time.sleep(1)    
+        self.driver.find_element_by_id('id_username').send_keys("luismi")
+        self.driver.find_element_by_id('id_password').send_keys("contraseña1",Keys.ENTER)
+        time.sleep(2) 
+        print('Logueando')
+        #In case of a correct loging, a element with id 'user-tools' is shown in the upper right part
+        print(len(self.driver.find_elements_by_id('user-tools')))
+        self.assertTrue(len(self.driver.find_elements_by_id('user-tools'))==1)       
+        
+    def tearDown(self):           
         self.driver.quit()
 
-        self.base.tearDown()
 
+
+if __name__ == '__main__':
+    unittest.main()
 
 
