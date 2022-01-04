5b1ffc188ef0bf448cba5f3b6c99f29ba633d3e6
luibelzan <luibelzan@outlook.com>
2022-01-04 20:29:37 +0100

    Test: Terminado test_startVoting (Visualizer)

--- decide/visualizer/tests.py
+++ decide/visualizer/tests.py
@@ -41,18 +41,18 @@ class AdminTestCase(StaticLiveServerTestCase):
         self.driver.find_element(by=By.XPATH, value="/html/body/div/div[3]/div/ul/li/a").click()
         self.driver.find_element_by_id('id_name').send_keys("Votacion de prueba")
         self.driver.find_element_by_id('id_desc').send_keys("Vamos a probar si funcionan los tests")
-        time.sleep(2)
+        time.sleep(1)
         #Almaceno la ventana de la votacion
         window_before = self.driver.window_handles[0]
         #Añadimos las opciones
         self.driver.find_element_by_id('add_id_question').click()
         #Almaceno la ventana de las opciones
         window_after = self.driver.window_handles[1]
-        time.sleep(2)
+        time.sleep(1)
         #Cambio de ventana
         self.driver.switch_to_window(window_after)
         self.driver.find_element_by_id('id_desc').send_keys("¿Funcionan las pruebas de decide?")
-        time.sleep(3)
+        time.sleep(1)
         self.driver.find_element_by_id('id_options-0-number').send_keys("1")
         self.driver.find_element_by_id('id_options-0-option').send_keys("Si")
         self.driver.find_element_by_id('id_options-1-number').send_keys("2")
@@ -60,7 +60,7 @@ class AdminTestCase(StaticLiveServerTestCase):
         self.driver.find_element_by_id('id_options-2-number').send_keys("3")
         self.driver.find_element_by_id('id_options-2-option').send_keys("Casi")
         self.driver.find_element(by=By.XPATH, value="/html/body/div/div[1]/div/form/div/div[2]/input").click()
-        time.sleep(3)    
+        time.sleep(1)    
         #Volvemos a la ventana de las votaciones
         self.driver.switch_to_window(window_before)
         window_before = self.driver.window_handles[0]
@@ -69,12 +69,23 @@ class AdminTestCase(StaticLiveServerTestCase):
         self.driver.switch_to_window(window_after)
         self.driver.find_element_by_id('id_name').send_keys("http://localhost:8000")
         self.driver.find_element_by_id('id_url').send_keys("http://localhost:8000")
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element_by_xpath('/html/body/div/div[1]/div/form/div/div/input').click()
         self.driver.switch_to_window(window_before)
-        time.sleep(3)
+        time.sleep(1)
         self.driver.find_element_by_xpath('/html/body/div/div[3]/div/form/div/div/input[1]').click()
         time.sleep(1)
+
+        #Iniciamos la votación
+        self.driver.find_element_by_xpath('/html/body/div/div[3]/div/div/form/div[2]/table/tbody/tr[1]/td[1]/input').click()
+        time.sleep(5)
+        self.driver.find_element_by_xpath('/html/body/div/div[3]/div/div/form/div[1]/label/select/option[3]').click()
+        self.driver.find_element_by_xpath('/html/body/div/div[3]/div/div/form/div[1]/button').click()
+        time.sleep(5)
+
+
+
+
 
     def tearDown(self):          
         super().tearDown()
