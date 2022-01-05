4240f15169fa1d1669f11372cd6bcf422f6b5914
Isamunval <isamunval@alum.us.es>
2022-01-05 21:09:25 +0100

    Fix: Corrección de error en django.yml y local_settings.py

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -13,24 +13,24 @@ MODULES = [
     'voting',
 ]
 
-APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
-}
+BASEURL = 'https://decide-penyagolosa.herokuapp.com/'
 
-BASEURL = 'http://localhost:8000'
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL
+}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'decide',
         'USER': 'decide',
         'PASSWORD': 'decide',
         'HOST': '127.0.0.1',
@@ -39,4 +39,5 @@ DATABASES = {
 }
 
 # number of bits for the key, all auths should use the same number of bits
+
 KEYBITS = 256
