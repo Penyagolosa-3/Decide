8f9da11f630a3bcd9cfcc6f19c187ff63c9213aa
marmarave <73103929+marmarave@users.noreply.github.com>
2022-01-06 14:28:53 +0100

    Update local_settings.py

--- decide/local_settings.py
+++ decide/local_settings.py
@@ -13,30 +13,31 @@ MODULES = [
     'voting',
 ]
 
-APIS = {
-    'authentication': 'http://localhost:8000',
-    'base': 'http://localhost:8000',
-    'booth': 'http://localhost:8000',
-    'census': 'http://localhost:8000',
-    'mixnet': 'http://localhost:8000',
-    'postproc': 'http://localhost:8000',
-    'store': 'http://localhost:8000',
-    'visualizer': 'http://localhost:8000',
-    'voting': 'http://localhost:8000',
-}
-
 BASEURL = 'http://localhost:8000'
+
+APIS = {
+    'authentication': BASEURL,
+    'base': BASEURL,
+    'booth': BASEURL,
+    'census': BASEURL,
+    'mixnet': BASEURL,
+    'postproc': BASEURL,
+    'store': BASEURL,
+    'visualizer': BASEURL,
+    'voting': BASEURL
+}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'decide',
-        'PASSWORD': 'complexpassword',
-        'HOST': 'localhost',
+        'PASSWORD': 'decide',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
 
 # number of bits for the key, all auths should use the same number of bits
+
 KEYBITS = 256
