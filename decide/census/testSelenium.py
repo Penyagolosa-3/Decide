216c3eecd6e7f6300275c46910c813efedda3697
marmarave <73103929+marmarave@users.noreply.github.com>
2022-01-06 15:04:55 +0100

    Update testSelenium.py

--- decide/census/testSelenium.py
+++ decide/census/testSelenium.py
@@ -8,10 +8,10 @@ from time import sleep
 options = webdriver.ChromeOptions()
 options.headless = True
 driver = webdriver.Chrome(options=options)
-driver.get("http://127.0.0.1:8000/admin/")
+driver.get("http://localhost:8000/admin/")
 driver.find_element_by_id('id_username').send_keys("decide")
 driver.find_element_by_id('id_password').send_keys("decide",Keys.ENTER)
-driver.get("http://127.0.0.1:8000/admin/census/census/")
+driver.get("http://localhost:8000/admin/census/census/")
 driver.find_element(By.CSS_SELECTOR, ".export_link").click()
 driver.find_element_by_id('id_file_format').send_keys(Keys.DOWN)
 driver.find_element(By.CSS_SELECTOR, ".default").click()
