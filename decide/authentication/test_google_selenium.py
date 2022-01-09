19a028cea21786ed71b184273fe2343db6f9b2d2
josgrarom <josgrarom@alum.us.es>
2022-01-09 17:04:04 +0100

    Test: Actualización de los test de google; #37 #38
    Se han solucionado algunos errores con los tests de login de google y votación.

--- decide/authentication/test_google_selenium.py
+++ decide/authentication/test_google_selenium.py
@@ -1,23 +1,23 @@
 
-# import pytest
-# import time
-# import json
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.action_chains import ActionChains
-# from selenium.webdriver.support import expected_conditions
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-# from base.tests import BaseTestCase
-# class TestPrueba():
+import pytest
+import time
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from base.tests import BaseTestCase
+class TestPrueba():
 
-  # def setup_method(self, method):
-  #   self.driver = webdriver.Chrome()
-  #   self.vars = {}
+  def setup_method(self, method):
+    self.driver = webdriver.Chrome()
+    self.vars = {}
   
-  # def teardown_method(self, method):
-  #   self.driver.quit()
+  def teardown_method(self, method):
+    self.driver.quit()
   
   # def test_ventanaDeLogeo(self):
   #   self.driver = webdriver.Chrome()
